1004b1bccba8fa99bcea66ff77512a91a8f0a734
Ezmoneysniper42 <93779199+Ezmoneysniper42@users.noreply.github.com>
2022-07-19 00:50:24 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+//It should display a h1 heading.
+
+ReactDOM.render(
+  <div>
+    <h1> This is a JSX code challange </h1>
+
+    <ul>
+      <li> Create and H1. </li>
+      <li> Create unordered list </li>
+      <li> Cover 3 elements </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
